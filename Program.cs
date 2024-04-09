7b8e7b28f1687e544ec815324fef1b7dfd0a9c79
Usuario <Usuario@CPD01-M16>
2024-04-09 10:56:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Despesas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp, desc;
+            double contV = 0, total;
+            Console.WriteLine("Descrição das despesas");
+            desc = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Valor: ");
+                contV = double.Parse(Console.ReadLine());
+                total = contV + contV;
+                Console.WriteLine("Deseja continuar?");
+                resp = Console.ReadLine();
+
+            }
+            while (resp == "s");
+            Console.WriteLine("Numero de pessoas na casa: ");
+            int numP = int.Parse(Console.ReadLine());
+            Console.WriteLine("Despesa em: " + desc);
+            Console.WriteLine("Depesa total: " + (total).ToString("C"));
+            Console.WriteLine("Media de gasto por pessoa: " +  (total)/numP);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Despesas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Despesas")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("46d17d9b-e49d-4f6c-8aa0-babd0f2b52bb")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
