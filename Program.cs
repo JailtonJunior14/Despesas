90b6de547b35b3865bf9a9ce954ef3067ed39413
JailtonJUnior14 <jailtonjrbr@gmail.com>
2024-04-16 10:37:17 -0300

    atualizado

--- Program.cs
+++ Program.cs
@@ -11,26 +11,23 @@ namespace Despesas
         static void Main(string[] args)
         {
             string resp, desc;
-            double contV = 0, total;
-            Console.WriteLine("Descrição das despesas");
-            desc = Console.ReadLine();
+            double contV = 0;
             do
             {
+                Console.WriteLine("Descrição das despesas");
+                desc = Console.ReadLine();
                 Console.WriteLine("Valor: ");
                 contV = double.Parse(Console.ReadLine());
-                total = contV + contV;
-                Console.WriteLine("Deseja continuar?");
+                Console.WriteLine("Numero de pessoas na casa: ");
+                int numP = int.Parse(Console.ReadLine());
+                Console.WriteLine("Despesa em: " + desc);
+                Console.WriteLine("Depesa total: " + (contV).ToString("C"));
+                Console.WriteLine("Media de gasto por pessoa: " + (contV) / numP);
+                Console.WriteLine("Deseja cadastrar uma nova despesa? S/N ");
                 resp = Console.ReadLine().ToLower();
 
             }
             while (resp == "s");
-            Console.WriteLine("Numero de pessoas na casa: ");
-            int numP = int.Parse(Console.ReadLine());
-            Console.WriteLine("Despesa em: " + desc);
-            Console.WriteLine("Depesa total: " + (total).ToString("C"));
-            Console.WriteLine("Media de gasto por pessoa: " +  (total)/numP);
-            Console.WriteLine("Deseja cadastrar uma nova despesa? S/N ");
-             resp = Console.ReadLine();
             Console.ReadKey();
         }
     }
